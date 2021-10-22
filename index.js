e65ed242c7b0e250560f2735bbfe8e460ac8fe63
TauH <thaywood988@gmail.com>
2021-10-22 11:40:39 +1100

    started tests and index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const Employee = require("./lib/Employee");
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+const addManager = () => {};
+const addEmployee = () => {};
